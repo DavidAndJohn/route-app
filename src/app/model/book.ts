76e483c4bcbcb5e9e114634f481fa67aacd3a85e
DavidAndJohn <63vidjohn@gmail.com>
2023-05-11 11:04:38 +0630

    just test

---
+++ src/app/model/book.ts
@@ -0,0 +1,14 @@
+export interface Book {
+  id?:number,
+  title?:string,
+  price?:number,
+  yearPublished:Date,
+  publisher?:string,
+  imagUrl?:string,
+  authorName?:string,
+  category?:{
+    id?:number;
+    name?:string
+  }
+
+}
